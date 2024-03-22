8b9c9327d895dfc96136e7f65b264783cc43d645
Your Name <you@example.com>
2024-03-22 11:21:02 +0100

    aze

--- web.py
+++ web.py
@@ -1,11 +1,72 @@
-from flask import Flask
-from markupsafe import escape
-app = Flask(__name__)
-#import pymysql as MySQLdb
+from apiflask import APIFlask, Schema, abort
+from apiflask.fields import Integer, String
+from apiflask.validators import Length, OneOf
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World2!'
+app = APIFlask(__name__)
+
+pets = [
+    {'id': 0, 'name': 'Kitty', 'category': 'cat'},
+    {'id': 1, 'name': 'Coco', 'category': 'dog'},
+    {'id': 2, 'name': 'Flash', 'category': 'cat'}
+]
+
+
+class PetIn(Schema):
+    name = String(required=True, validate=Length(0, 10))
+    category = String(required=True, validate=OneOf(['dog', 'cat']))
+
+
+class PetOut(Schema):
+    id = Integer()
+    name = String()
+    category = String()
+
+@app.get('/')
+def say_hello():
+    return {'message': 'Hello!'}
+
+
+@app.get('/pets/<int:pet_id>')
+@app.output(PetOut)
+def get_pet(pet_id):
+    if pet_id > len(pets) - 1 or pets[pet_id].get('deleted'):
+        abort(404)
+    return pets[pet_id]
+
+
+@app.get('/pets')
+@app.output(PetOut(many=True))
+def get_pets():
+    return pets
+
+@app.post('/pets')
+@app.input(PetIn, location='json')
+@app.output(PetOut, status_code=201)
+def create_pet(json_data):
+    pet_id = len(pets)
+    json_data['id'] = pet_id
+    pets.append(json_data)
+    return pets[pet_id]
+
+
+@app.patch('/pets/<int:pet_id>')
+@app.input(PetIn(partial=True), location='json')
+@app.output(PetOut)
+def update_pet(pet_id, json_data):
+    if pet_id > len(pets) - 1:
+        abort(404)
+    for attr, value in json_data.items():
+        pets[pet_id][attr] = value
+    return pets[pet_id]
+
+@app.delete('/pets/<int:pet_id>')
+@app.output({}, status_code=204)
+def delete_pet(pet_id):
+    if pet_id > len(pets) - 1:
+        abort(404)
+    pets[pet_id]['deleted'] = True
+    pets[pet_id]['name'] = 'Ghost'
+    return ''
 
 @app.route('/sql')
 def sql():
@@ -24,11 +85,6 @@ def sql():
     except:
         return "ERROR IN CONNECTION"
 
-
-@app.route('/hello/<username>')
-def hello_user(username):
-    # say hello to that user
-    return 'Hello %s' % escape(username)
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080, debug=True)
